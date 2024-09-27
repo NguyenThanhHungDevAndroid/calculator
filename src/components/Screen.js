9297cc7c730db3af4dc4392415ce2c7d085abd19
nguyenthanhhung <nguyenthanhhung.dev@gmail.com>
2024-09-27 16:08:20 +0700

    Update tính năng tính liên tục,

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,32 +1,39 @@
-import React, { useRef, useEffect } from "react";
+import React, {useRef, useEffect} from "react";
 import "./Screen.css";
 
-const Screen = ({ value }) => {
-  const textRef = useRef(null);
+const Screen = ({value}) => {
+    const textRef = useRef(null);
 
-  useEffect(() => {
-    const adjustFontSize = () => {
-      const element = textRef.current;
-      let fontSize = 70;
+    useEffect(() => {
+        const adjustFontSize = () => {
+            const element = textRef.current;
+            let fontSize = 70;
 
-      element.style.fontSize = `${fontSize}px`;
+            element.style.fontSize = `${fontSize}px`;
 
-      while (element.scrollWidth > element.clientWidth && fontSize > 1) {
-        fontSize--;
-        element.style.fontSize = `${fontSize}px`;
-      }
-    };
+            while (element.scrollWidth > element.clientWidth && fontSize > 1) {
+                fontSize--;
+                element.style.fontSize = `${fontSize}px`;
+            }
+        };
 
-    adjustFontSize();
-  }, [value]);
+        adjustFontSize();
+    }, [value]);
 
-  return (
-    <div className="screen">
-      <p className="responsive-text" ref={textRef}>
-        {value}
-      </p>
-    </div>
-  );
+    let displayValue = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    if (displayValue.length > 15) {
+        displayValue = `${displayValue.slice(0, 9)}...`;
+    }
+
+
+    return (
+        <div className="screen">
+            <p className="responsive-text" ref={textRef}>
+                {displayValue}
+                {/*{value}*/}
+            </p>
+        </div>
+    );
 };
 
 export default Screen;
